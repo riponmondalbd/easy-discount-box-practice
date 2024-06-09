ab3491597b9ccf0f55770a8dcf436b35e40eb3c6
riponmondalbd <riponmondal149@gmial.com>
2024-06-10 02:31:53 +0600

    make nan validation also alart system

--- js/discount.js
+++ js/discount.js
@@ -2,11 +2,17 @@ document.getElementById('btn-apply').addEventListener('click', function () {
     const priceField = document.getElementById('input-field');
     const newPriceFieldString = priceField.value;
     const newPriceField = parseFloat(newPriceFieldString);
-
-    const discount = newPriceField - (newPriceField * 0.3);
-
     const payField = document.getElementById('pay-field');
-    payField.innerText = discount;
+
+    if (isNaN(newPriceField)) {
+        alert('Please provide a number');
+        return;
+    }
+    else {
+        const discount = newPriceField - (newPriceField * 0.3);
+        payField.innerText = discount;
+    }
+
 
     // clear input field value
     priceField.value = '';
